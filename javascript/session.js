195765f87701554459adf37b0433a400d377b380
humanediedofanxiety <saysayeem.pr@gmail.com>
2025-02-07 23:14:29 +0600

    done some work

---
+++ javascript/session.js
@@ -0,0 +1,17 @@
+console.log("So what is the plan?");
+console.log("I'll list down some filthy short-term goals");
+
+let short_term = [
+  "1.Complete 30-days of JS (30 questions)",
+  "2.Get the javascript certificate (basics+intermediate)",
+  "3.learn more JS (intermediate-advanced)",
+];
+
+let hacker_rank = [
+  "1.Complete preparation module 'Algorithms'",
+  "2.Complete preparation module 'Data structures'",
+  "3.Acquire certificate 'Problem Solving (basic)'",
+  "4.Acquire certificate 'Problem solving (intermediate)",
+];
+
+console.log(short_term, hacker_rank);
