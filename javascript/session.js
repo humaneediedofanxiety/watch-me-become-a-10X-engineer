41a17e157184d07de5aa66fd90977789d44e9531
humanediedofanxiety <saysayeem.pr@gmail.com>
2025-02-08 00:03:04 +0600

    coded a bit

--- javascript/session.js
+++ javascript/session.js
@@ -2,7 +2,7 @@ console.log("So what is the plan?");
 console.log("I'll list down some filthy short-term goals");
 
 let short_term = [
-  "1.Complete 30-days of JS (30 questions)",
+  "1.Complete 30-days of JS (27 questions left) -> current",
   "2.Get the javascript certificate (basics+intermediate)",
   "3.learn more JS (intermediate-advanced)",
 ];
